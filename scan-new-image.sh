662d1a1f866894471f7be27beacb52a9af16b466
Ken McCann <ken.mccann@aquasec.com>
2020-11-17 14:26:24 -0700

    Modified curl to use v2, and fewer params.

--- scan-new-image.sh
+++ scan-new-image.sh
@@ -9,28 +9,10 @@
 # Input Parameters:
 #   1: URL
 #   2: Aqua Registry Name
-#   3: Repository Name
-#   4: Image Tag
+#   3: image:tag
 #
 
-#DEBUG
-echo AQUA USER: $AQUA_USER
-echo AQUA PASS: $AQUA_PASS
-
-#DEBUG
-echo curl -s -k -u $AQUA_USER:$AQUA_PASS -X POST -H 'Content-Type:application/json' $1/api/v1/images --data
-
-curl -s -k -u $AQUA_USER:$AQUA_PASS -X POST -H 'Content-Type:application/json' $1/api/v1/images --data @<(cat <<EOF
-{
-  "images": [
-    {
-      "registry": "$2",
-      "repository": "$3",
-      "tag": "$4",
-      "digest": null,
-      "source": null
-    }
-  ]
-}
-EOF
-)
+curl -vvv "$1/api/v2/images" \
+  -u $AQUA_USER:$AQUA_PASS \
+  -H 'Content-Type:application/json' \
+  --data-binary '{"registry": "ACR","image": "'"$3"'"}' 
